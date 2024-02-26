ef4635ccea4fb3f36b0867862b737f8c401dadb3
mhr1380 <mhr.1380.2001@gmail.com>
2024-02-27 03:10:09 +0330

    remove api folder

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,9 @@ export default function Document() {
         name="viewport"
         content="width=device-width, initial-scale=1"
       ></meta>
+      <meta name="description" content="منوی دیجیتال کافه سیسیلیا"></meta>
+      <title>کافه سیسیلیا</title>
+      <link rel="icon" href="/logo-black.png" sizes="any" />
       <body>
         <Main />
         <NextScript />
